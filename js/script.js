bf07befbc672b96316f0fe1442907165fd7ffb34
janunezbe-design <janunezbe@gmail.com>
2025-10-08 15:28:36 -0300

    add script file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//alert("Hola Mundo");
+const button = document.getElementById("toggle-theme")
+//alert(button);
+
+function toggleTheme(){
+    const body= document.body;
+    //alert(body)
+    if(body.classList.contains("dark")){
+        body.classList.remove("dark");
+    }else{
+        body.classList.add("dark")
+    }
+}
+
+button.addEventListener("click",toggleTheme)
